e4fa3b0c20b769477e7b62dd44fa47ff69068b0c
Daniel-vdL <alexanderdanailin@gmail.com>
2024-02-22 14:52:02 +0100

    Created database connection

--- SimFit360/Model/AppDbContext.cs
+++ SimFit360/Model/AppDbContext.cs
@@ -1,12 +1,23 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SimFit360.Model
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Go to the App.config.example file and then follow Instructions
+
+            optionsBuilder.UseMySql(
+                ConfigurationManager.ConnectionStrings["name"].ConnectionString,
+                ServerVersion.Parse("5.7.33-winx64"));
+        }
+
     }
 }
